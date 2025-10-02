33c02d5ea7ea0a903dd722ab3cfd44e7cb379003
ArnoutAllaert <allaertarnout@gmail.com>
2025-10-02 14:32:04 +0200

    this one should work too

---
+++ src/python/tenant_setup.py
@@ -0,0 +1,51 @@
+import os
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
+def generate_secret(namespace: str, key: str) -> str:
+    return f"{namespace}-{key}-S3cr3t!"
+
+
+def create_tenant_db(
+    tenant: str,
+    postgres_host: str,
+    postgres_username: str,
+    postgres_password: str,
+):
+    conn = psycopg2.connect(
+        database="postgres",
+        host=postgres_host,
+        port=5432,
+        user=postgres_username,
+        password=postgres_password,
+    )
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+
+    db_password = generate_secret(tenant, "db_password")
+
+    try:
+        cur.execute(
+            f'CREATE ROLE "{tenant}" NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN NOREPLICATION '
+            f'CONNECTION LIMIT 20 ENCRYPTED PASSWORD \'{db_password}\' ROLE "{postgres_username}";'
+        )
+        cur.execute(
+            f'CREATE DATABASE "{tenant}" WITH OWNER = "{tenant}" ENCODING = \'UTF8\';'
+        )
+        cur.execute(
+            f'GRANT CONNECT, TEMP ON DATABASE "{tenant}" TO "{tenant}";'
+        )
+    finally:
+        cur.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    tenant = input("Tenant/namespace: ")
+    host = os.getenv("PGHOST", "localhost")
+    user = os.getenv("PGUSER", "postgres")
+    pwd = os.getenv("PGPASSWORD", "postgres")
+
+    create_tenant_db(tenant, host, user, pwd)
+    print("Done.")
